4c6d1938af8745c333e06cf9b02cb449d59c3a78
Vladyslav Salenik <vladyslav.salenik@gmail.com>
2024-02-01 11:54:11 +0200

    ;

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function getElementWidth(content, padding, border) {
-    return parseInt(content) + parseInt(padding) * 2 + parseInt(border) * 2
-}
+    return parseInt(content) + parseInt(padding) * 2 + parseInt(border) * 2;
+};
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
